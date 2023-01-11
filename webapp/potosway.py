642717790a5c832b247df8b2fa099322dc3a1f69
Gérard FARAND <gfarand@gmail.com>
2023-01-11 14:36:09 +0100

    webapp test 1

--- webapp/potosway.py
+++ webapp/potosway.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python
 
+import Adafruit_DHT
 from flask import Flask, request
 from gpiozero import LED
 
 app = Flask(__name__)
 led = LED(17)
+dht_pin = 21
+dht = Adafruit_DHT.DHT11
+
+
+
+@app.route('/')
+def index():
+  ( _humidity, _celsius ) = Adafruit_DHT.read_retry( dht, dht_pin )
+  'title' = 'Potosway Webapp'
 
 @app.route('/led/on', methods=['POST'])
 def led_on():
@@ -18,3 +28,20 @@ def led_off():
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
+
+
+
+import Adafruit_DHT
+from time import sleep
+p=print # Alias 
+# Note: Use &gt;=2000ms polling intervals
+poll_interval = 2 # Seconds
+dht_pin = 21 # BCM24/BOARD18
+  
+if __name__ == '__main__':
+  while True: 
+    ( _humidity, _celsius ) = Adafruit_DHT.read_retry( _dht, _dht_pin )
+    p( "Humidity =&gt; %.1f%% RH" % _humidity ) 
+    p( "Temperature =&gt; %.2fF" % _celsius )
+    p( "%.2fC" % _celsius )
+    sleep( _poll_interval )
