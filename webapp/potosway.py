ee37fad185b00a373a52c8a7e532a73508854ce3
Gérard FARAND <gfarand@gmail.com>
2023-01-15 09:55:32 +0100

    separated led and rgb actions

--- webapp/potosway.py
+++ webapp/potosway.py
@@ -10,6 +10,12 @@ led       = LED(17)
 rgb       = RGBLED(13,19,6)
 dht_pin   = 21
 
+def rgb_on():
+    rgb.color = (1,1,1)
+
+def rgb_off():
+    rgb.color = (0,0,0)
+
 def disco_time():
   while True: 
     zzz     = round(random.uniform(.1,.5),1) 
@@ -34,17 +40,28 @@ def index():
     return render_template('potosway.html', **templateData)
 
 @app.route("/<deviceName>/<action>")
-def action(deviceName, action):
+def led_action(deviceName, action):
     if deviceName == "led":
         device = led
-    if deviceName == "rgb":
-        device = rgb
     if action == "on":
         device.on()
     if action == "off":
         device.off()
     if action == "blink":
         device.blink(.5, .3)
+    templateData = {
+        'led_status' : str(led.is_lit)
+    }
+    return render_template('potosway.html', **templateData)
+
+@app.route("/<deviceName>/<action>")
+def rgb_action(deviceName, action):
+    if deviceName == "rgb":
+        device = rgb
+    if action == "on":
+        rgb_on()
+    if action == "off":
+        rgb_off()
     if action == "disco":
         disco_time()
     templateData = {
