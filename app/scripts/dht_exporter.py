b20a8337e3d5da7808ccd303f209ace08ff6cd2e
Gérard FARAND <gfarand@gmail.com>
2023-01-16 10:18:58 +0100

    string

--- app/scripts/dht_exporter.py
+++ app/scripts/dht_exporter.py
@@ -6,13 +6,13 @@ from time import sleep
 dht_model   = 11
 dht_pin     = 21
 scrape      = 15 #Unit is seconds
-temp        = Gauge('dht_temperature', 'Temperature in celsius')
-hum         = Gauge('dht_humidity', 'Humidity in percent')
+temp        = Gauge('dht_temperature_celsius', 'Temperature in celsius')
+hum         = Gauge('dht_humidity_percent', 'Humidity in percent')
 
 start_http_server(8000)
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(dht_model, dht_pin)
-    temp.set(temp)
-    hum.set(hum)
+    temp.set(dht_temperature_celsius)
+    hum.set(dht_humidity_percent)
     sleep(scrape)
 
