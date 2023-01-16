e3d6b1ef3fa980c648f5fe374a4386844fe52e84
Gérard FARAND <gfarand@gmail.com>
2023-01-16 10:00:39 +0100

    string

--- app/scripts/dht_exporter.py
+++ app/scripts/dht_exporter.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 import Adafruit_DHT
-from prometheus_client import start_http_server, Summary
+from prometheus_client import start_http_server, Gauge
 from time import sleep
 
 dht_model   = 11
 dht_pin     = 21
 scrape      = 15 #Unit is seconds
-temp        = Summary('dht_temperature', 'Temperature in celsius')
-hum         = Summary('dht_humidity', 'Humidity in percent')
+temp        = Gauge('dht_temperature', 'Temperature in celsius')
+hum         = Gauge('dht_humidity', 'Humidity in percent')
 
 start_http_server(8000)
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(dht_model, dht_pin)
     temp.observe(temp)
     hum.observe(hum)
     sleep(scrape)
